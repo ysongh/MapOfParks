ca68b9b5691ffba346200172a2aeb81eef64c6d0
You Song Hou <yousonghou@yahoo.com>
2020-07-16 00:34:35 -0400

    remove geolocated

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,35 +1,23 @@
 import React, { Component } from 'react';
-import { Map, Marker, TileLayer, Popup } from 'react-leaflet';
+import { Map, TileLayer } from 'react-leaflet';
 import { geolocated } from "react-geolocated";
 import axios from 'axios';
 
 import MarkerList from './MarkerList';
-import { homeIcon } from './Icons';
 import { API1 } from '../config';
 
 class MapView extends Component{
     state = {
         coordinatesList: [],
-        lat: 40.0423477,
-        lng: -73.8082212,
+        lat: 41.2423477,
+        lng: -73.9082212,
         zoom: 8.5,
     }
 
-    timer() {
-        if(this.props.coords){
-            this.setState({
-                lat: this.props.coords.latitude,
-                lng: this.props.coords.longitude
-            })
-            clearInterval(this.intervalId);
-        }
-    }
-
     async componentDidMount() {
-        this.intervalId = setInterval(this.timer.bind(this), 1000);
-
         try{
             const data = await axios.get(API1);
+            console.log(data)
 
             this.setState({ coordinatesList: data.data });
         } catch(err){
@@ -39,19 +27,6 @@ class MapView extends Component{
 
     render(){
         const position = [this.state.lat, this.state.lng];
-        const location = (
-            !this.props.isGeolocationAvailable ? (
-                <div>Your browser does not support Geolocation</div>
-            ) : !this.props.isGeolocationEnabled ? (
-                <div>Geolocation is not enabled</div>
-            ) : this.props.coords ? (
-                    <Marker position={[this.props.coords.latitude, this.props.coords.longitude]} icon={homeIcon}>
-                        <Popup>
-                            Your Location
-                        </Popup>
-                    </Marker>
-            ) : null
-        )
         return (
             <div>
                 <h1>Map</h1>
@@ -62,7 +37,6 @@ class MapView extends Component{
                     />
                     
                     <MarkerList coordinatesList={this.state.coordinatesList} />
-                    {location}
                 </Map>
             </div>
             
