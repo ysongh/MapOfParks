11e78989e54fc689c6ec0cb7f0abcb3deee0bcd6
You Song Hou <yousonghou@yahoo.com>
2020-07-17 00:28:50 -0400

    display a direction to get to that station location using the API

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 import MarkerLocationList from './MarkerLocationList';
 import MarkerTrainList from './MarkerTrainList';
-import { API1 } from '../data/config';
+import { APIKEY, API1, API2 } from '../data/config';
 import { trainData } from '../data/data';
 
 class MapView extends Component{
@@ -14,6 +14,7 @@ class MapView extends Component{
         lat: 41.2423477,
         lng: -73.9082212,
         zoom: 8.5,
+        direction: ""
     }
 
     async componentDidMount() {
@@ -26,20 +27,38 @@ class MapView extends Component{
         }
     }
 
+    async getDirections(e){
+        const id = e.sourceTarget.options.value;
+        console.log(id);
+
+        const data = await axios.get(API2 + id + "/" + APIKEY + "station.json");
+
+        this.setState({ direction: data.data.GetStationDetailJsonResult.Directions1 });
+    }
+
     render(){
         const position = [this.state.lat, this.state.lng];
         return (
             <div>
                 <h1>Map</h1>
+
+                {this.state.direction ? (
+                    <div className="directionBox">
+                        <h6>Direction:</h6>
+                        <p>{this.state.direction}</p>
+                    </div>
+                ) : null}
+                
                 <Map className="map" center={position} zoom={this.state.zoom}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
                     
-                    <MarkerLocationList coordinatesList={this.state.coordinatesList} />
+                    <MarkerLocationList coordinatesList={this.state.coordinatesList} getDirections={this.getDirections.bind(this)} />
                     <MarkerTrainList coordinatesList={trainData} />
                 </Map>
+                
             </div>
             
         )
--- src/components/MarkerLocationList.js
+++ src/components/MarkerLocationList.js
@@ -3,12 +3,12 @@ import { Marker, Popup } from 'react-leaflet';
 
 import { locationIcon } from './Icons';
 
-const MarkerList = ({ coordinatesList }) => {
+const MarkerList = ({ coordinatesList, getDirections }) => {
     return(
         <div>
             {coordinatesList.map(cl => {
                 return(
-                    <Marker key={cl.Code} position={[cl.Latitude, cl.Longitude]} icon={locationIcon}>
+                    <Marker key={cl.Code} position={[cl.Latitude, cl.Longitude]} icon={locationIcon} onClick={getDirections} value={cl.Code}>
                         <Popup>
                             <h6>{cl.Name}</h6>
                         </Popup>
