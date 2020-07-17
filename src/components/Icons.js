e0ec222e43ce1d268bce6033612f09dba5e14bf2
You Song Hou <yousonghou@yahoo.com>
2020-07-16 20:00:15 -0400

    add more icons

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -3,6 +3,8 @@ import L from 'leaflet';
 import IconBlue from '../images/icon1.svg';
 import IconRed from '../images/icon2.svg';
 import IconHome from '../images/home.svg';
+import IconLocation from '../images/location.svg';
+import IconTrain from '../images/train.svg';
 
 export const homeIcon = L.icon({
     iconUrl: IconHome,
@@ -22,5 +24,19 @@ export const redIcon = L.icon({
     iconUrl: IconRed,
     iconRetinaUrl: IconRed,
     iconSize: [25, 41],
+    popupAnchor: [0, -10]
+});
+
+export const locationIcon = L.icon({
+    iconUrl: IconLocation,
+    iconRetinaUrl: IconLocation,
+    iconSize: [10, 10],
+    popupAnchor: [0, -10]
+});
+
+export const trainIcon = L.icon({
+    iconUrl: IconTrain,
+    iconRetinaUrl: IconTrain,
+    iconSize: [20, 20],
     popupAnchor: [0, -10]
 });
